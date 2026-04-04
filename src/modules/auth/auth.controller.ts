3f3dd5a32eefd4c164f739b57f6ec927a88eeeb5
Your Name <dev.milon923@gmail.com>
2026-04-04 22:01:06 +0600

    prisma setup done and tested dummy create oparation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from "express";
 import { handleAsync } from "../../utils/handleAsync";
-import ServerError from "../../utils/error";
-
+import { prisma } from "../../utils/prisma";
+import sendResponse from "../../utils/response";
+import httpStatus from "http-status";
 const register = handleAsync(async (req: Request, res: Response) => {
-  throw new ServerError(400, "errorf");
+  const user = await prisma.user.create({
+    data: {
+      email: "elsa@prisma.io",
+      name: "Elsa Prisma",
+    },
+  });
+  return sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "Requst send successfully!",
+    data: user,
+  });
 });
 export const AuthController = {
   register,
