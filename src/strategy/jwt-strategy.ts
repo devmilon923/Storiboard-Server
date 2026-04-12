651425b732ae5b2ff5b9d0a32fcd9a7d156ecf92
Your Name <dev.milon923@gmail.com>
2026-04-12 20:30:41 +0600

    update passport strategy

--- src/strategy/jwt-strategy.ts
+++ src/strategy/jwt-strategy.ts
@@ -1,29 +1,16 @@
-import { Request } from "express";
 import passport from "passport";
-import { Strategy as JwtStrategy } from "passport-jwt";
-
-// 1. Custom function to pull JWT from the cookie
-const cookieExtractor = (req: Request) => {
-  let token = null;
-  console.log(req.cookies);
-  if (req && req.cookies) {
-    // Replace 'token' with the name you used in res.cookie()
-    token = req.cookies["jt"];
-  }
-  return token;
-};
+import { ExtractJwt, Strategy as JwtStrategy } from "passport-jwt";
 
 const secret = process.env.JWT_SECRET_KEY;
 
 const opts: any = {
-  // 2. Tell Passport to use the cookie instead of the header
-  jwtFromRequest: cookieExtractor,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: secret,
 };
-
+// opts.issuer = "accounts.examplesoft.com";
+// opts.audience = "yoursite.net";
 passport.use(
   new JwtStrategy(opts, (jwt_payload: any, done: any) => {
-    // Passport will now only call this if the cookie token is valid
     return done(null, jwt_payload);
   }),
 );
