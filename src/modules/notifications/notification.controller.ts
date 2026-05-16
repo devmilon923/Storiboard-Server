17dbe22a25e095f8d95aff0fc88a1b10d9146b08
devmilon923 <dev.milon923@gmail.com>
2026-05-16 23:12:53 +0600

    feat: implement cursor-based pagination for fetching user notifications in NotificationController

--- src/modules/notifications/notification.controller.ts
+++ src/modules/notifications/notification.controller.ts
@@ -1,9 +1,40 @@
 import { Request, Response } from "express";
 import { handleAsync } from "../../utils/handleAsync";
-
-const getAllNotifications = handleAsync(
-  async (req: Request, res: Response) => {},
-);
+import { prisma } from "../../utils/prisma";
+import { TJwtUser } from "../../utils/jwtValidation";
+import sendResponse from "../../utils/response";
+import httpStatus from "http-status";
+const getAllNotifications = handleAsync(async (req: Request, res: Response) => {
+  const pc = Number(req.query.pc as string) || null;
+  const limit = Number(req.query.limit as string) || 10;
+  const user = req.user as TJwtUser;
+  console.log(limit);
+  const result = await prisma.notifications.findMany({
+    take: limit,
+    skip: pc ? 1 : 0,
+    cursor: pc ? { id: pc } : undefined,
+    where: { receiverId: user.id },
+    include: {
+      sender: {
+        select: {
+          id: true,
+          image: true,
+          name: true,
+        },
+      },
+    },
+    orderBy: { id: "desc" },
+  });
+  console.log(result);
+  const cursor = result.length === limit ? result[result.length - 1].id : null;
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Get all notifications successfully!",
+    data: result,
+    cursor: cursor,
+  });
+});
 export const NotificationController = {
   getAllNotifications,
 };
